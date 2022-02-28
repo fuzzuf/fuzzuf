19099e9fc745906190fd20e3c046e2c3325a9d88
mkYYY <15130103+mkYYY@users.noreply.github.com>
2022-02-28 18:29:56 +0900

    Use AFL_ROOT instead

--- cmake/FindFridaTrace.cmake
+++ cmake/FindFridaTrace.cmake
@@ -1,13 +1,35 @@
-if ( EXISTS "${CMAKE_BINARY_DIR}/afl-frida-trace.so" )
-  message("[*] afl-frida-trace.so already exists.")
-elseif ( DEFINED ENABLE_FRIDA_TRACE )
-  message("[+] Building afl-frida-trace.so from AFLplusplus submodule...")
-  execute_process(
-    COMMAND ${CMAKE_SOURCE_DIR}/scripts/build_frida_trace.sh
-      ${CMAKE_SOURCE_DIR}/AFLplusplus
-      ${CMAKE_BINARY_DIR}
-    COMMENT "Setting up frida mode..."
-    DEPENDS ${CMAKE_SOURCE_DIR}/scripts/build_frida_trace.sh
-    VERBATIM
-  )
+#
+# fuzzuf
+# Copyright (C) 2022 Ricerca Security
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see http://www.gnu.org/licenses/.
+#
+# FIXME: This cmake file does not consider cross-compile
+if( "${CMAKE_SYSTEM_PROCESSOR}" STREQUAL "x86_64" )
+  if ( DEFINED AFL_ROOT )
+    find_program(
+      FRIDA_TRACE_SO
+      afl-frida-trace.so
+      HINTS ${AFL_ROOT}/
+    )
+  else()
+    message( STATUS "afl-frida-trace.so location is not specified." )
+  endif()
+  if ( DEFINED FRIDA_TRACE_SO )
+    file(
+      COPY ${FRIDA_TRACE_SO}
+      DESTINATION ${CMAKE_BINARY_DIR}
+    )
+  endif()
 endif()
--- docs/frida_mode.md
+++ docs/frida_mode.md
@@ -7,18 +7,21 @@ Currently, there are limitations on using Frida mode on fuzzuf:
 - Only available on x86-64 Linux through CLI.
 - [Scripting](https://github.com/AFLplusplus/AFLplusplus/blob/stable/frida_mode/Scripting.md) is not available
 
+## Build instruction and example
 
-## Build instruction
+To use Frida mode, first, you have to clone AFL++ by yourself, and build `afl-frida-trace.so` by yourself.  
+Then, specify AFL++'s path to `-DAFL_ROOT` while generating a CMake's build system.  
 
-To use Frida mode, specify `-DENABLE_FRIDA_TRACE=1` while generating a CMake's build system.  
-It will clone `AFLplusplus` submodule and build `afl-frida-trace.so` located under `AFLplusplus/frida_mode/` directory.
+Use `--frida` flag to enable/disable a frida mode (disabled by default).
 
 ```console
+$ git clone https://github.com/AFLplusplus/AFLplusplus.git
+$ make -C AFLplusplus/frida_mode
 $ cd /path/to/fuzzuf
+$ cmake -B build -DAFL_ROOT=/path/to/AFLplusplus # specify other options as required
+$ cmake --build build -j`nproc`
 $ # Build plain (dynamically linked, uninstrumented) `exifutil` executable
 $ make -C docs/resources/exifutil clean all
-$ cmake -B build ... -DENABLE_FRIDA_TRACE=1
-$ cmake --build build -j`nproc`
 $ cd build
 $ # Currently, frida mode is available for afl and aflfast
 $ ./fuzzuf [afl|aflfast] -i ../docs/resources/exifutil/fuzz_input --frida=1 -- \
