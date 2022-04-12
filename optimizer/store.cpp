65238ae2632edb252134979928b8d26abae0e655
hugeh0ge <yukik@ricsec.co.jp>
2022-04-12 14:55:19 +0900

    fix wrong include

--- optimizer/store.cpp
+++ optimizer/store.cpp
@@ -15,7 +15,8 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see http://www.gnu.org/licenses/.
  */
-#include "fuzzuf/optimizer/store.cpp"
+
+#include "fuzzuf/optimizer/store.hpp"
 
 namespace fuzzuf::optimizer {
 
