f67dcc34e5244f1ee04d6331d2d331e195231a6b
Naomasa Matsubayashi <naomasam@ricsec.co.jp>
2022-06-10 11:56:44 +0900

    Rename fuzzuf-afl-cc++ to fuzzuf-afl-c++

--- cmake/FindFuzzufCC.cmake
+++ cmake/FindFuzzufCC.cmake
@@ -1,10 +1,10 @@
 if(NOT FUZZUF_CC_ROOT)
   find_program( FUZZUF_CC_AFL_CC fuzzuf-afl-cc )
-  find_program( FUZZUF_CC_AFL_CXX fuzzuf-afl-cc++ )
+  find_program( FUZZUF_CC_AFL_CXX fuzzuf-afl-c++ )
   find_program( FUZZUF_CC_IJON_CC fuzzuf-ijon-cc )
 else()
   find_program( FUZZUF_CC_AFL_CC fuzzuf-afl-cc NO_DEFAULT_PATH PATHS ${FUZZUF_CC_ROOT}/ ${FUZZUF_CC_ROOT}/bin/ )
-  find_program( FUZZUF_CC_AFL_CXX fuzzuf-afl-cc++ NO_DEFAULT_PATH PATHS ${FUZZUF_CC_ROOT}/ ${FUZZUF_CC_ROOT}/bin/ )
+  find_program( FUZZUF_CC_AFL_CXX fuzzuf-afl-c++ NO_DEFAULT_PATH PATHS ${FUZZUF_CC_ROOT}/ ${FUZZUF_CC_ROOT}/bin/ )
   find_program( FUZZUF_CC_IJON_CC fuzzuf-ijon-cc NO_DEFAULT_PATH PATHS ${FUZZUF_CC_ROOT}/ ${FUZZUF_CC_ROOT}/bin/ )
 endif()
 FIND_PACKAGE_HANDLE_STANDARD_ARGS(
