38bf964d8cce5b5f65c4ccd97a1d3675e191f0c2
icchy <ryoi@ricsec.co.jp>
2022-07-19 15:55:09 +0900

    format

--- algorithms/mopt/mopt_state.cpp
+++ algorithms/mopt/mopt_state.cpp
@@ -1,26 +1,28 @@
 #include "fuzzuf/algorithms/mopt/mopt_state.hpp"
-#include "fuzzuf/utils/random.hpp"
+
 #include "fuzzuf/algorithms/mopt/mopt_option.hpp"
 #include "fuzzuf/algorithms/mopt/mopt_setting.hpp"
+#include "fuzzuf/utils/random.hpp"
 
 namespace fuzzuf::algorithm::mopt {
 
 MOptState::MOptState(
     std::shared_ptr<const MOptSetting> setting,
     std::shared_ptr<executor::AFLExecutorInterface> executor,
-    std::unique_ptr<optimizer::Optimizer<u32>>&& mutop_optimizer
-) : afl::AFLStateTemplate<MOptTestcase>(setting, executor, std::move(mutop_optimizer)),
-    setting(setting)
-{
-    UpdateSpliceCycles(); // init
+    std::unique_ptr<optimizer::Optimizer<u32>>&& mutop_optimizer)
+    : afl::AFLStateTemplate<MOptTestcase>(setting, executor,
+                                          std::move(mutop_optimizer)),
+      setting(setting) {
+  UpdateSpliceCycles();  // init
 }
 
 MOptState::~MOptState() {}
 
 void MOptState::UpdateSpliceCycles() {
-    splice_cycles_limit = fuzzuf::utils::random::Random<u32>(option::GetSpliceCyclesLow<option::MOptTag>(), option::GetSpliceCyclesUp<option::MOptTag>());
+  splice_cycles_limit = fuzzuf::utils::random::Random<u32>(
+      option::GetSpliceCyclesLow<option::MOptTag>(),
+      option::GetSpliceCyclesUp<option::MOptTag>());
 }
-
 
 void MOptState::ShowStats(void) {
   // Lots of constants appear, so overlook this.
--- include/fuzzuf/algorithms/mopt/mopt_state.hpp
+++ include/fuzzuf/algorithms/mopt/mopt_state.hpp
@@ -2,35 +2,34 @@
 
 #include <memory>
 
+#include "fuzzuf/algorithms/afl/afl_state.hpp"
+#include "fuzzuf/algorithms/mopt/mopt_optimizer.hpp"
+#include "fuzzuf/algorithms/mopt/mopt_setting.hpp"
+#include "fuzzuf/algorithms/mopt/mopt_testcase.hpp"
 #include "fuzzuf/executor/native_linux_executor.hpp"
 #include "fuzzuf/optimizer/optimizer.hpp"
-#include "fuzzuf/algorithms/afl/afl_state.hpp"
-#include "fuzzuf/algorithms/mopt/mopt_setting.hpp"
-#include "fuzzuf/algorithms/mopt/mopt_optimizer.hpp"
-#include "fuzzuf/algorithms/mopt/mopt_testcase.hpp"
 
 namespace fuzzuf::algorithm::mopt {
 
 struct MOptState : public afl::AFLStateTemplate<MOptTestcase> {
-    explicit MOptState(
-        std::shared_ptr<const mopt::MOptSetting> setting,
-        std::shared_ptr<executor::AFLExecutorInterface> executor,
-        std::unique_ptr<optimizer::Optimizer<u32>>&& mutop_optimizer
-    );
-    ~MOptState();
+  explicit MOptState(
+      std::shared_ptr<const mopt::MOptSetting> setting,
+      std::shared_ptr<executor::AFLExecutorInterface> executor,
+      std::unique_ptr<optimizer::Optimizer<u32>>&& mutop_optimizer);
+  ~MOptState();
 
-    void UpdateSpliceCycles();
+  void UpdateSpliceCycles();
 
-    void ShowStats(void);
+  void ShowStats(void);
 
-    bool pacemaker_mode = false; // key_puppet: (0: false, 1: true)
-    bool core_mode = false; // key_module: (0: pilot, 1: core)
+  bool pacemaker_mode = false;  // key_puppet: (0: false, 1: true)
+  bool core_mode = false;       // key_module: (0: pilot, 1: core)
 
-    u32 splice_cycles_limit = 0;
+  u32 splice_cycles_limit = 0;
 
-    std::unique_ptr<fuzzuf::optimizer::MOptOptimizer> mopt;
+  std::unique_ptr<fuzzuf::optimizer::MOptOptimizer> mopt;
 
-    std::shared_ptr<const MOptSetting> setting;
+  std::shared_ptr<const MOptSetting> setting;
 };
 
-} // namespace fuzzuf::algorithm::mopt
+}  // namespace fuzzuf::algorithm::mopt
