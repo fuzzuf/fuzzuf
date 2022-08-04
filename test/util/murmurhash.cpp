e95b3dc2daa8630db204ecf67f222797f9079abb
M6M <minhok@ricsec.co.jp>
2022-08-04 13:06:39 +0900

    Add a test for xxhash and rename the original to imply murmurhash

--- test/util/murmurhash.cpp
+++ test/util/murmurhash.cpp
@@ -22,15 +22,15 @@
 #include <boost/test/unit_test.hpp>
 #include "fuzzuf/utils/common.hpp"
 #include "random_data.hpp"
-BOOST_AUTO_TEST_CASE(UtilHash32) {
+BOOST_AUTO_TEST_CASE(UtilMurmurHash32) {
 #ifdef __x86_64__
-  BOOST_CHECK_EQUAL( ( fuzzuf::utils::Hash32( random_data1.data(), random_data1.size(), 0xa5b35705 ) ), 1150639252 );
-  BOOST_CHECK_EQUAL( ( fuzzuf::utils::Hash32( random_data2.data(), random_data2.size(), 0xa5b35705 ) ), 1783865212 );
-  BOOST_CHECK_EQUAL( ( fuzzuf::utils::Hash32( random_data3.data(), random_data3.size(), 0xa5b35705 ) ), 4021282027 );
+  BOOST_CHECK_EQUAL( ( fuzzuf::utils::MurmurHash32( random_data1.data(), random_data1.size(), 0xa5b35705 ) ), 1150639252 );
+  BOOST_CHECK_EQUAL( ( fuzzuf::utils::MurmurHash32( random_data2.data(), random_data2.size(), 0xa5b35705 ) ), 1783865212 );
+  BOOST_CHECK_EQUAL( ( fuzzuf::utils::MurmurHash32( random_data3.data(), random_data3.size(), 0xa5b35705 ) ), 4021282027 );
 #else
-  BOOST_CHECK_EQUAL( ( fuzzuf::utils::Hash32( random_data1.data(), random_data1.size(), 0xa5b35705 ) ), 1260228967 );
-  BOOST_CHECK_EQUAL( ( fuzzuf::utils::Hash32( random_data2.data(), random_data2.size(), 0xa5b35705 ) ), 665174276 );
-  BOOST_CHECK_EQUAL( ( fuzzuf::utils::Hash32( random_data3.data(), random_data3.size(), 0xa5b35705 ) ), 2392848746 );
+  BOOST_CHECK_EQUAL( ( fuzzuf::utils::MurmurHash32( random_data1.data(), random_data1.size(), 0xa5b35705 ) ), 1260228967 );
+  BOOST_CHECK_EQUAL( ( fuzzuf::utils::MurmurHash32( random_data2.data(), random_data2.size(), 0xa5b35705 ) ), 665174276 );
+  BOOST_CHECK_EQUAL( ( fuzzuf::utils::MurmurHash32( random_data3.data(), random_data3.size(), 0xa5b35705 ) ), 2392848746 );
 #endif
 }
 
